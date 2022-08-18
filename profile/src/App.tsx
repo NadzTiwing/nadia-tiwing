3ed27aee682423e5ef03f642d5476ee5151376b8
nadz <mc4nadia@gmail.com>
2022-08-18 15:37:39 +0800

    change react bootstrap

--- profile/src/App.tsx
+++ profile/src/App.tsx
@@ -1,20 +1,26 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
-import Home from "./home"
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Home/>
-  </ChakraProvider>
-)
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
